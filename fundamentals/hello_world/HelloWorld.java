5dfa4a19334d194af9eeca235e44d041260fd0c7
kleelof <kleelof@yahoo.com>
2022-03-10 18:21:50 -0800

    updates

--- fundamentals/hello_world/HelloWorld.java
+++ fundamentals/hello_world/HelloWorld.java
@@ -1,6 +1,123 @@
+import java.util.*;
 public class HelloWorld {
 
     public static void main(String[] args) {
+
+        /*
+            Arrays
+                - A fixed order list of same type data
+                - can easily access any item in the list
+                - immutable
+        */
+
+        String[] daysOfTheWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        System.out.println(daysOfTheWeek);
+
+        System.out.println(daysOfTheWeek[2]);
+
+        daysOfTheWeek[4] = "Bon Jovi Friday!";
+        daysOfTheWeek[5] = "FriYay!";
+
+        for ( int x = 0; x < daysOfTheWeek.length; x ++ ) {
+            System.out.println(daysOfTheWeek[x]);
+        }
+
+        System.out.println("------------------");
+
+        String[] newDays = new String[daysOfTheWeek.length + 1];
+        for ( int x = 0; x < daysOfTheWeek.length; x ++ )  newDays[x] = daysOfTheWeek[x];
+        newDays[newDays.length - 1] = "NANDay"; // add the new item
+
+        /* 
+            ArrayList
+                - similar to an Array
+                - mutable
+        */
+
+        ArrayList<String>shoppingList = new ArrayList<String>();
+
+        shoppingList.add("eggs");
+        shoppingList.add("tortillas");
+        shoppingList.add(1, "cheese"); // insert
+        
+        for ( String item: shoppingList) {
+            System.out.println(item);
+        }
+
+        /*
+            {
+                "name": "Fred Flintstone",
+                "age": "35",
+                "profession": "Manager"
+            }
+
+            {
+                "name": "Wilma Flintstone",
+                "age": "42",
+                "profession": "CEO"
+            }
+        */
+
+        HashMap<String, String> userRecord = new HashMap<String, String>();
+        userRecord.put("name", "Fred Flintstone");
+        userRecord.put("age", "35");
+        userRecord.put("profession", "manager");
+
+        System.out.println(userRecord.get("age"));
+
+        for(String key : userRecord.keySet()) {
+            System.out.printf("%s: %s\n", key, userRecord.get(key));
+        }
+
+        /* 
+           Array / ArrayList vs. HashMap
+               - Arrays and ArrayLists for keeping an ordered list of items.
+               - Hashmap for accessing specific information in the structure
+
+               - Array and ArrayList keys are auto generated as numbers
+               - Hashmap keys are assigned by the code and are usually numbers or string.
+       */
+        
+       /*
+            methods -
+                - what?
+                    - block of code that does one thing and does that one thing well
+                - why?
+                    - OOP design patterns we will cover next week
+        */
+
+        System.out.println("------------------");
+        System.out.println("------------------");
+
+        sayHello();
+
+        // can receive and return data
+        String flavor = "vanilla";
+        System.out.printf(
+            "Fred %s %s.\n", 
+            checkIfLikesFlavor(flavor) ? "likes" : "doesn't like", 
+            flavor
+        );
+
+        // overloading
+        sayHello("Wilma");
+    }
+
+    public static void sayHello() {
+
+        System.out.println("Hello");
+    }
+
+    public static void sayHello(String name) {
+        System.out.printf("Hello %s! \n", name);
+    }
+
+    public static boolean checkIfLikesFlavor(String flavor) {
+        return flavor == "chocolate";
+    }
+
+    public static void lessonOne() {
+
         
         System.out.println("Hello World!");
         System.out.println("------------------");
@@ -29,7 +146,7 @@ public class HelloWorld {
 
         // type; camelCase; semi-colon
         int itemCount = 100;
-        double priceEach = 1.14;
+        Double priceEach = 1.14;
 
         double subTotal = itemCount * priceEach;
 
@@ -83,6 +200,21 @@ public class HelloWorld {
             deliveryFee = 10;
         } else {
             deliveryFee = 1;
+        }
+
+        // switch statement
+        String animal = "dog";
+
+        switch(animal) {
+            case("dog"):
+                System.out.println("arf arf");
+            break;
+            case("cat"):
+                System.out.println("feed me! NOW!");
+            break;
+            case("pig"):
+                System.out.println("oot oot");
+            break;
         }
 
         System.out.printf("Your delivery fee is: %d", deliveryFee);
