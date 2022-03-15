8ca5e12dbd41505b090cc79a8224a6923774d75f
kleelof <kleelof@yahoo.com>
2022-03-15 15:55:06 -0700

    updates

---
+++ fundamentals/zoo_app/ZooApp.java
@@ -0,0 +1,8 @@
+
+
+public class ZooApp {
+    
+    public static void main(String[] args) {
+       
+    }
+}
---
+++ fundamentals/zoo_app/classes/Animal.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Animal {
+
+    public static int numberOfAnimals = 0;
+
+    private String classification;
+    private String name;
+    private String type;
+
+}
