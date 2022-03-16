ad427e22d8f46e45a88b4e8312c825de74a88429
kleelof <kleelof@yahoo.com>
2022-03-15 18:34:18 -0700

    updates

--- fundamentals/zoo_app/ZooApp.java
+++ fundamentals/zoo_app/ZooApp.java
@@ -1,8 +1,40 @@
-
+import classes.Animal;
 
 public class ZooApp {
     
     public static void main(String[] args) {
        
+        lessonOne();
+    }
+
+    public static void lessonOne() {
+
+        /*
+            Classes
+                - abstraction
+                - blueprint
+                - create discreet and easy to manage objects
+                - 2 things
+                    - attributes
+                    - methods
+        */
+
+        Animal leo = new Animal("mammal", "lion", "Leo"); // instantiation
+        Animal frankie = new Animal("amphibian", "frog", "Frankie");
+
+        // breakpoint
+        System.out.println("------------------");
+
+        // Zoo is growing, increasing zookeepers from 1 to 3 ( overloading constructor )
+        // defaults in other languages
+        Animal petra = new Animal("bird", "parrot", "Petra", "Barney");
+
+        System.out.println("------------------");
+
+        petra.move("South");
+        System.out.println(petra.getName());
+        
+        System.out.println(Animal.numberOfAnimals);
+        
     }
 }
--- fundamentals/zoo_app/classes/Animal.java
+++ fundamentals/zoo_app/classes/Animal.java
@@ -8,4 +8,56 @@ public class Animal {
     private String name;
     private String type;
 
+    private String zooKeeper = "Fred";
+
+    private int energy = 50; // 0 - 100
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getClassification() {
+        return this.classification;
+    }
+
+    public String getZookeeper() {
+        return this.zooKeeper;
+    }
+
+    public void setZookeeper(String zookeeper) {
+        this.zooKeeper = zookeeper;
+    }
+
+    public void setEnergy(int energy) {
+        if ( energy > 100 ) energy = 100;
+
+        this.energy = energy;
+    }
+
+
+    // constructor
+    public Animal(String classification, String type, String name ) {
+
+        this.name = name;
+        this.type = type;
+        this.classification = classification;
+
+        Animal.numberOfAnimals ++;
+    }
+
+    // overload constructor
+    public Animal(String classification, String type, String name, String zooKeeper) {
+
+        this(classification, type, name);
+
+        this.zooKeeper = zooKeeper;
+    }
+
+    public void move(String direction) {
+        System.out.printf("%s is moving %s.\n", this.name, direction);
+    }
 }
